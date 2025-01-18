d3b0d8e970352024a2ca2791530e4892c87928a2
riekoonitsuka <rieko631209@gmail.com>
2025-01-18 21:01:34 +0900

    ブックマーク機能

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "dotenv-rails"
 gem "cancancan"
 gem "rails_admin"
 gem "sassc-rails"
+gem "turbo-rails"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,10 @@ class BookmarksController < ApplicationController
   def create
     @practice = Practice.find(params[:practice_id])
     current_user.bookmark(@practice)
-    redirect_to practices_path, success: "Bookmarked"
   end
 
   def destroy
     @practice = current_user.bookmarks.find(params[:id]).practice
     current_user.unbookmark(@practice)
-    redirect_to practices_path, success: "unbookmarked", status: :see_other
   end
 end
